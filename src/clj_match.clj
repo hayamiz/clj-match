f88979d38658fa27971c12865b75ac79ed2da2fd
Yuto Hayamizu <y.hayamizu@gmail.com>
2010-05-25 13:48:01 +0900

    use defn- and letfn for internal functions

--- src/clj_match.clj
+++ src/clj_match.clj
@@ -2,28 +2,48 @@
   (:use [clojure core set]
         [clojure.contrib str-utils pprint]))
 
+(defn- find-symbols-inner [pat]
+  (cond
+   (symbol? pat)
+   (condp = pat
+     '_ {}
+     pat {pat true})
+   ;;
+   (and (coll? pat)
+	(= (count pat) 2)
+	(= 'quote (first pat)))
+   {}
+   ;;
+   (and (coll? pat)
+	(= (count pat) 2)
+	(= '& (first pat))
+	(= (symbol? (second pat))))
+   {(second pat) true}
+   ;;
+   (and (coll? pat)
+	(> (count pat) 0)
+	(or (= :and (first pat))
+	    (= :or (first pat))
+	    (= :not (first pat))))
+   (apply
+    union
+    (map find-symbols-inner (rest pat)))
+   ;;
+   (and (coll? pat)
+	(> (count pat) 1)
+	(= :? (first pat)))
+   (apply
+    union
+    (map find-symbols-inner (nthnext pat 2)))
+   ;;
+   (coll? pat)
+   (cond
+    (empty? pat) {}
+    true (union (find-symbols-inner (first pat))
+		(find-symbols-inner (rest pat))))
+   true {}))
+
 (defn find-symbols [pat]
-  (defn find-symbols-inner [pat]
-    (cond
-     (symbol? pat)	(if (= '_ pat) {} {pat true})
-     ;;
-     (and (coll? pat)
-	  (> (count pat) 0)
-	  (or (= :and (first pat))
-	      (= :or (first pat))))
-     (apply
-      union
-      (map find-symbols-inner (rest pat)))
-     ;;
-     (and (coll? pat)
-	  (> (count pat) 1)
-	  (= :? (first pat)))
-     (apply
-      union
-      (map find-symbols-inner (nthnext pat 2)))
-     ;;
-     (coll? pat)	(apply union (map find-symbols-inner pat))
-     true {}))
   (keys (find-symbols-inner pat)))
 
 (defn compile-clause [val pat]
@@ -84,3 +104,38 @@
 	   (let ~deref-bindings
 	     ~match-body)
 	   ~@mismatch-body)))))
+
+(defmacro match [val & clauses]
+  (let [clauses (partition 2 clauses),
+	val-sym (gensym)]
+    (letfn [(make-lazy-clauses
+	     [clauses]
+	     (if (empty? clauses)
+	       '(empty [])
+	       (let [clause (first clauses),
+		     rest (rest clauses),
+		     [pat body] clause,
+		     syms (find-symbols pat),
+		     pred (compile-clause val-sym pat),
+		     ref-bindings (apply
+				   vector
+				   (apply concat
+					  (map (fn [sym] `[~sym (ref nil)])
+					       syms))),
+		     deref-bindings (apply
+				     vector
+				     (apply concat
+					    (map (fn [sym] `[~sym @~sym])
+						 syms)))
+		     ]
+		 `(lazy-seq
+		   (cons
+		    (let ~ref-bindings
+		      (if ~pred
+			[true (let ~deref-bindings ~body)]
+			[false nil]))
+		    ~(make-lazy-clauses rest))))))]
+      (let [lazy-clauses (make-lazy-clauses clauses)]
+	`(let [~val-sym ~val]
+	   (some (fn [[matched?# ret#]] (and matched?# ret#))
+		 ~lazy-clauses))))))
