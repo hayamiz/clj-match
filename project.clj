281ece32124d376f38d6d795526359d047fd7e78
Yuto Hayamizu <y.hayamizu@gmail.com>
2010-05-25 13:47:30 +0900

    add license and description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clj-match "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject clj-match "0.0.1-SNAPSHOT"
+  :description "A port of Andrew Wright's pattern match library for Clojure."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
