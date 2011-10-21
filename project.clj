ecb8ac2e8c0284947cb4ff9f6f1de5cbbdb0faca
Yuto Hayamizu <y.hayamizu@gmail.com>
2011-10-21 22:09:56 +0900

    version 0.0.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-match "0.0.4-SNAPSHOT"
+(defproject clj-match "0.0.5-SNAPSHOT"
   :description "A port of Andrew Wright's pattern match library for Clojure."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
